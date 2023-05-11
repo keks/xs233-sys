1e3b9197ec05efd8ce7658d5b39427963592626e
Jan Winkelmann (keks) <git@keks.cool>
2023-05-11 16:07:58 +0200

    build: update --init submodule

--- build.rs
+++ build.rs
@@ -7,6 +7,13 @@ fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let bindings_path = out_path.join("bindings.rs");
 
+    // fetch c-xs233 code
+    std::process::Command::new("git")
+      .args(["submodule", "update", "--init", "--depth", "1", "--recommend-shallow"])
+      .output()
+      .expect("error fetching c-xs233 repository as a submodule");
+
+
     // build c-xs233.
     // the build directives are based on the makefile.
     cc::Build::new()
